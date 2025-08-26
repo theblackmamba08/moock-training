cfe9aa8cadc9226c761b252e0ef20a46ec650805
theblackmamba08 <fossocabrel08@gmail.com>
2025-08-26 11:39:29 +0100

    courseinfo, étape 9

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,39 +1,72 @@
 import Course from "./components/Course"
 
 const App = () => {
-	const course = {
-		id : 1,
-		name :'Half Stack application development',
-  		parts : [
-    		{
-      			name: 'Fundamentals of React',
-      			exercises: 10,
-				id : 1
-    		}
-    		,{
-      			name: 'Using props to pass data',
-      			exercises: 7,
-				id : 2
-    		}
-    		,{
-      			name: 'State of a component',
-      			exercises: 14,
-				id : 3
-    		}
-			,{
-      			name: 'Redux',
-      			exercises: 11,
-				id : 4
-    		}
-			,{
-      			name: 'GraphQL',
-      			exercises: 8,
-				id : 5
-    		}
-  		]
-	} 
+	const courses = [
+		{
+			id : 1,
+			name :'Half Stack application development',
+			parts : [
+				{
+					name: 'Fundamentals of React',
+					exercises: 10,
+					id : 1
+				}
+				,{
+					name: 'Using props to pass data',
+					exercises: 7,
+					id : 2
+				}
+				,{
+					name: 'State of a component',
+					exercises: 14,
+					id : 3
+				}
+				,{
+					name: 'Redux',
+					exercises: 11,
+					id : 4
+				}
+				,{
+					name: 'GraphQL',
+					exercises: 8,
+					id : 5
+				}
+			]
+		},
+		{
+			id : 2,
+			name :'Full Stack application development',
+			parts : [
+				{
+					name: 'Fundamentals of React',
+					exercises: 10,
+					id : 1
+				}
+				,{
+					name: 'Reusing props to pass data',
+					exercises: 7,
+					id : 2
+				}
+				,{
+					name: 'Refactoring a component',
+					exercises: 14,
+					id : 3
+				}
+				,{
+					name: 'Deployment',
+					exercises: 11,
+					id : 4
+				}
+				,{
+					name: 'NodeJS',
+					exercises: 8,
+					id : 5
+				}
+			]
+		} 
+	]
 
-	return <Course course={course} />
+	return courses.map( c => <Course key={c.id} course={c} /> )
 }
 //   return (
 //     <>
--- part1/courseinfo/src/components/Total.jsx
+++ part1/courseinfo/src/components/Total.jsx
@@ -1,6 +1,6 @@
 const Total = ({course}) => {
     const total = course.parts.reduce( (s, p) => s + p.exercises , 0)
-    return <p>Number of exercises {total}</p>
+    return <p>Total of {total} exercises</p>
 }
 
 export default Total
