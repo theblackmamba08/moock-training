ed1cda68bc114eff87a623031cdaf50bf685bb58
theblackmamba08 <fossocabrel08@gmail.com>
2025-08-27 11:34:15 +0100

    petite amelioration du code de courseinfo-project

--- courseinfo-project/src/App.jsx
+++ courseinfo-project/src/App.jsx
@@ -68,37 +68,5 @@ const App = () => {
 
 	return courses.map( c => <Course key={c.id} course={c} /> )
 }
-//   return (
-//     <>
-//       <Header course={course} />
-//       <Content course={course} />
-//       <Total course={course} />
-//     </>
-//   )
-// }
-
-
-// const Header = (props) => {
-//   console.log(props)
-//   return <h1>{props.course.name}</h1>
-// }
-
-// const Content = (props) => {
-//   return (
-//     <>
-//       <Part part={props.course.parts[0]} />
-//       <Part part={props.course.parts[1]} />
-//       <Part part={props.course.parts[2]} />
-//     </>
-//   )
-// }
-
-// const Part = (props) => {
-//   return <p>{props.part.name} {props.part.exercises}</p>
-// }
-
-// const Total = (props) => {
-//   return <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
-// }
 
 export default App
